d11b1fc50524044be2cf9d5ece45ed509612052a
Muhammad <muhammad_bw_26@mail.ru>
2023-05-08 12:08:55 +0500

    Common commets

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+// function for sending email
 function sendMail(){
+    // vocabluary for having an access to HTML elemets 
     var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         message: document.getElementById("message").value
     }
 
+    // My ID's for email
     const servieID = "service_ai280au"
     const templateID = "template_60qz6c1"
 
     
+    // sending email and returning HTML elemets to default value
     emailjs.send(servieID, templateID, params)
     .then(
+        // if email was send without ERRORS
         res =>{
             document.getElementById("name").value = ""
             document.getElementById("email").value = ""
@@ -19,13 +24,7 @@ function sendMail(){
             alert("Mail was sent")
         }
     )
-
+        // if there were some ERRORS
     .catch(arr => console.log(err))
     
 }
-
-
-function test(){
-    console.log("This is WORK")
-}
-
