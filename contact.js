0f157db1ecbc8007c5295d9dcc52d0cb868e883c
Muhammad <muhammad.bw.03@gmail.com>
2023-05-17 22:47:55 +0500

    maded some small changes

---
+++ contact.js
@@ -0,0 +1,30 @@
+// function for sending email
+function sendMail(){
+    // vocabluary for having an access to HTML elemets 
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    }
+
+    // My ID's for email
+    const servieID = "service_ai280au"
+    const templateID = "template_60qz6c1"
+
+    
+    // sending email and returning HTML elemets to default value
+    emailjs.send(servieID, templateID, params)
+    .then(
+        // if email was send without ERRORS
+        res =>{
+            document.getElementById("name").value = ""
+            document.getElementById("email").value = ""
+            document.getElementById("message").value = ""
+
+            alert("Mail was sent")
+        }
+    )
+        // if there were some ERRORS
+    .catch(arr => console.log(err))
+    
+}
--- index.js
+++ index.js
@@ -6,38 +6,27 @@ if (html.clientWidth < 800){
 }
 
 
+document.addEventListener("DOMContentLoaded", ()=>{
+    const tetris = document.querySelectorAll('.card');
+    const root = document.documentElement;
 
-
-// function for sending email
-function sendMail(){
-    // vocabluary for having an access to HTML elemets 
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    }
-
-    // My ID's for email
-    const servieID = "service_ai280au"
-    const templateID = "template_60qz6c1"
-
-    
-    // sending email and returning HTML elemets to default value
-    emailjs.send(servieID, templateID, params)
-    .then(
-        // if email was send without ERRORS
-        res =>{
-            document.getElementById("name").value = ""
-            document.getElementById("email").value = ""
-            document.getElementById("message").value = ""
-
-            alert("Mail was sent")
+tetris.forEach(card =>{
+    card.addEventListener("mouseover", ()=>{
+    root.style.setProperty('--project_back', '#101010');
+             })
         }
     )
-        // if there were some ERRORS
-    .catch(arr => console.log(err))
-    
-}
+
+tetris.forEach(card =>{
+    card.addEventListener("mouseleave", ()=>{
+    root.style.setProperty('--project_back', '#101010');
+            })
+        }
+    )
+
+})
+
+
 
 
 
