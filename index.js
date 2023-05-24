b393057b0fe11a5389f4cb99fdb4d999fd2b3403
Muhammad <muhammad.bw.03@gmail.com>
2023-05-24 23:25:03 +0500

    tried to make more adaptive

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-let html = document.documentElement;
-console.log(html.clientWidth);
-if (html.clientWidth < 800){
-    alert("small screen");
-    window.location.href = 'SmallScreen.html';
-}
+// let html = document.documentElement;
+// console.log(html.clientWidth);
+// if (html.clientWidth < 800){
+//     alert("small screen");
+//     window.location.href = 'SmallScreen.html';
+// }
 
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    const tetris = document.querySelectorAll('.card');
-    const root = document.documentElement;
+// document.addEventListener("DOMContentLoaded", ()=>{
+//     const tetris = document.querySelectorAll('.card');
+//     const root = document.documentElement;
 
-tetris.forEach(card =>{
-    card.addEventListener("mouseover", ()=>{
-    root.style.setProperty('--project_back', '#101010');
-             })
-        }
-    )
+// tetris.forEach(card =>{
+//     card.addEventListener("mouseover", ()=>{
+//     root.style.setProperty('--project_back', '#101010');
+//              })
+//         }
+//     )
 
-tetris.forEach(card =>{
-    card.addEventListener("mouseleave", ()=>{
-    root.style.setProperty('--project_back', '#101010');
-            })
-        }
-    )
+// tetris.forEach(card =>{
+//     card.addEventListener("mouseleave", ()=>{
+//     root.style.setProperty('--project_back', '#101010');
+//             })
+//         }
+//     )
 
-})
+// })
 
 
 
